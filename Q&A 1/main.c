11b5570f86d93ad855c1aa5b50ecebc596713ab5
ctpune <40361654+ctpune@users.noreply.github.com>
2024-03-30 21:57:39 -0600

    Add files via upload

---
+++ Q&A 1/main.c
@@ -0,0 +1,221 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int name,z,n;
+    printf("****WELCOME TO THE QUIZ!****\n");
+    printf("Please enter your name to proceed:\n");
+    scanf("%s",&name);
+    printf("Which among them is a sniper rifle?:\n1.Mosin Nagant\n2.M4A4\n3.AK 47\n4.Desert Eagle\n");
+    printf("Type the option number:\n");
+    scanf("%d",&z);
+
+    if(z==1)
+
+        printf("You are correct!\n");
+        else
+{
+
+
+           printf("It's wrong try again!\n");
+return 0;
+}
+printf("Which among them is a place in France?:\n1.\Waterloo\n2.Amsterdam\n3.Normandy\n4.Hiroshima\n");
+    printf("Type the option number:\n");
+    scanf("%d",&n);
+
+    if(n==3)
+
+        printf("You are correct again!\n");
+        else
+{
+
+
+           printf("It's wrong try again!\n");
+return 0;
+}
+ printf("How many white squares are present in a chess board?:\n1.16\n2.32\n3.64\n4.24\n");
+    printf("Type the option number:\n");
+    scanf("%d",&z);
+
+    if(z==2)
+
+        printf("Good momentum!\n");
+        else
+{
+
+
+           printf("It's wrong try again!\n");
+return 0;
+}
+ printf("Where will FIFA World Cup 2022 be held?:\n1.Japan\n2.Qatar\n3.China\n4.Australia\n");
+    printf("Type the option number:\n");
+    scanf("%d",&z);
+
+    if(z==2)
+
+        printf("Keep it up!\n");
+        else
+{
+
+
+           printf("It's wrong try again!\n");
+return 0;
+}
+ printf("Who was the runner up of the T20 world cup 2016?:\n1.England\n2.New Zealand\n3.Sri Lanka\n4.India\n");
+    printf("Type the option number:\n");
+    scanf("%d",&z);
+
+    if(z==1)
+
+        printf("Great!\n");
+        else
+{
+
+
+           printf("It's wrong try again!\n");
+return 0;
+}
+ printf("Who was the President of USA during World War II?:\n1.George Bush\n2.Franklin Roosevelt\n3.John Kennedy\n4.Bill Clinton\n");
+    printf("Type the option number:\n");
+    scanf("%d",&z);
+
+    if(z==2)
+
+        printf("Yes you are absolutely right!\n");
+        else
+{
+
+
+           printf("It's wrong try again!\n");
+return 0;
+}
+ printf("Who won the Copa America 2016:\n1.Mexico\n2.USA\n3.Brazil\n4.Chile\n");
+    printf("Type the option number:\n");
+    scanf("%d",&z);
+
+    if(z==4)
+
+        printf("You got this question right!\n");
+        else
+{
+
+
+           printf("It's wrong try again!\n");
+return 0;
+}
+    printf("How many questions did you answer till now?:\n1.6\n2.8\n3.7\n4.5\n");
+    printf("Type the option number:\n");
+    scanf("%d",&z);
+
+    if(z==3)
+
+        printf("You know everything!\n");
+        else
+{
+
+
+           printf("It's wrong try again!\n");
+return 0;
+}
+printf("Who won all the matches FIFA qualifiers 2018:\n1.Germany\n2.France\n3.Brazil\n4.Uruguay\n");
+    printf("Type the option number:\n");
+    scanf("%d",&z);
+
+    if(z==1)
+
+        printf("AWESOME!\n");
+        else
+{
+
+
+           printf("It's wrong try again!\n");
+return 0;
+}
+printf("Which among them is the fastest land vehicle?:\n1.\Hennessey Venom GT \n2.Koenigsegg Agera R \n3.Bugatti Veyron Super Sport\n4.Thrust SSC\n");
+    printf("Type the option number:\n");
+    scanf("%d",&n);
+
+    if(n==4)
+
+        printf("GREAT!\n");
+        else
+{
+
+
+           printf("It's wrong try again!\n");
+return 0;
+}
+printf("Name of the football used in FIFA 2018 Russia:\n1.\Adidas Brazuca\n2.Adidas Telstar 18 \n3.Adidas Telstar 19\n4.Adidas Jabulani\n");
+    printf("Type the option number:\n");
+    scanf("%d",&n);
+
+    if(n==2)
+
+        printf("Good Thinking!!\n");
+        else
+{
+
+
+           printf("It's wrong try again!\n");
+return 0;
+}
+printf("Who is the youngest chess grandmaster till now:\n1.\Magnus Carlsen\n2.Sergey Karjakin \n3.Fabiano Caruana\n4.Bobby Fischer\n");
+    printf("Type the option number:\n");
+    scanf("%d",&n);
+
+    if(n==2)
+
+        printf("Unbelievable!\n");
+        else
+{
+
+
+           printf("It's wrong try again!\n");
+return 0;
+}
+printf("Who won WWE Money in the bank 2015:\n1.\Randy Orton\n2.Seth Rollins\n3.Sheamus\n4.John Cena\n");
+    printf("Type the option number:\n");
+    scanf("%d",&n);
+
+    if(n==3)
+
+        printf("Wow Man!\n");
+        else
+{
+
+
+           printf("It's wrong try again!\n");
+return 0;
+}
+ printf("Who won the 2006 FIFA World Cup? :\n1.France\n2.Brazil\n3.Italy\n4.Germany\n");
+    printf("Type the option number:\n");
+    scanf("%d",&z);
+
+    if(z==3)
+
+        printf("Keep it up!!\n");
+        else
+{
+
+
+           printf("It's wrong try again!\n");
+return 0;
+}
+printf("Who got a red card in 2006 FIFA World Cup? :\n1.Marco Materazzi\n2.Zinedine Zidane\n3.David Beckham\n4.Miroslav Klose\n");
+    printf("Type the option number:\n");
+    scanf("%d",&z);
+
+    if(z==2)
+
+        printf("You are great\n");
+        else
+{
+
+
+           printf("It's wrong try again!\n");
+return 0;
+}
+    return 0;
+}
